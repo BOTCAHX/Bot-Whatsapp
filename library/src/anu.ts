8abe38259b361574d653ece961d00e07bd3147a4
I`am Ra <78637964+rayyreall@users.noreply.github.com>
2021-10-17 19:44:34 +0700

    Delete anu.ts

--- library/src/anu.ts
+++
@@ -1,8 +0,0 @@
-import { HandlingData } from '../typings';
-import { ClientMessage } from '../Base/Scripts/client';
-
-
-
-export var Coy = globalThis.Client.on("coy", (data: HandlingData, Cli: ClientMessage) => {
-	console.log('woi')
-}, { event: ["li babi"], command: "oi", withPrefix: false, tag: "ajg"})
