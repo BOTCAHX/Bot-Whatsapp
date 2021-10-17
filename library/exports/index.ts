b46262a9ba7b8a5568c5a9e7b6871a9556a79148
I`am Ra <78637964+rayyreall@users.noreply.github.com>
2021-10-17 20:04:25 +0700

    Example Exports

--- library/exports/index.ts
+++ library/exports/index.ts
@@ -1,10 +1,39 @@
 import { HandlingData, getCommand } from '../typings';
 import { ClientMessage } from '../Base/Scripts/client';
 
+/**
+ * [id description]
+ *
+ * @var {[type]}
+ */
+/*
+* [data]
+data is used to retrieve event data such as from, id, args, prefix, etc.
+
+you can also see the data types in the file handler.d.ts
+on HandlingData
+*/ 
+/*
+* [Cli]
+Cli is used for functions like reply, sendFile etc
+or you can also look in the client.ts file
+in the Base/Script Folder
+*/ 
 let handle = (data: HandlingData, Cli: ClientMessage) => {
 	return void Cli.reply(data.from, "Test Command Export", data.id)
 }
 
+/**
+ * [testExpor description]
+ *
+ * @var {[type]}
+This is used for concatenating events you can also look in the typings folder
+in file handler.d.ts
+
+in the getCommand
+
+to see what data is needed
+ */
 export let testExpor: getCommand = {
 	event: ["halo"],
 	tag: "test",
